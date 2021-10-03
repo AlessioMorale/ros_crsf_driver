b4861444b3c17542229f8a39e87fa8ce2c8664d5
Alessio Morale <alessiomorale@gmail.com>
2021-10-03 13:17:40 +0100

    fix: add setup.py and related config

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(
+    name="crsf_drv",
+    version="1.0",
+    description="Python Distribution Utilities",
+    author="Alessio Morale",
+    author_email="alessiomorale@gmail.com",
+    packages=["crsf_drv"],
+    package_dir={"": "crsf_drv"},
+)
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['crsf_drv'],
+    scripts=['scripts/crsf_drv_node.py'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
