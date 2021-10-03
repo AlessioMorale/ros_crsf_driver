b3eb0a1d5e98293e4c1150533fc0c003f781a96b
Alessio Morale <alessiomorale@gmail.com>
2021-10-03 13:19:41 +0100

    feat: Add ros node script

--- scripts/crsf_drv_node.py
+++ scripts/crsf_drv_node.py
@@ -1,1 +1,29 @@
-from crsf_drv.crsf_drv import CRSFDrv
+from crsf_drv.crsf_drv import CRSFConfiguration, CRSFDrv
+from sensor_msgs.msg import Joy
+import rospy
+
+
+def main():
+    """
+    CRSF Driver node wrapper
+    """
+    rospy.init_node("CRSF joy emulation driver")
+    rospy.loginfo("CRSF joy emulation driver starting")
+    publisher = rospy.Publisher("~/joy", Joy, queue_size=10)
+    config = CRSFConfiguration(
+        rospy.get_param("~mapping/axis", default=[]),
+        rospy.get_param("~mapping/buttons", default=[]),
+        rospy.get_param("~serial/port"),
+        rospy.get_param("~serial/baudrate", 420000),
+        rospy.get_param("~joy_message_rate", 50),
+        rospy.get_param("~failsafe/timeout", 0.5),
+        rospy.get_param("~failsafe/axis", [0, 0, 0, 0]),
+        rospy.get_param("~failsafe/buttons", [0, 0, 0, 0])
+    )
+
+    rospy.loginfo(config)
+    crsf_drv = CRSFDrv(config, publisher)
+    rospy.loginfo("CRSF joy emulation driver running")
+    crsf_drv.run()
+
+main()
